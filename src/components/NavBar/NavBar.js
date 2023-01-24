d2c0845e61efdaf95e8301fe997446a4bcaba1a9
martinsosafer <martin_sos@gmail.com>
2023-01-24 14:48:37 -0300

    medianet

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav, NavBarContainer, NavLogo } from "./NavBarElements";
+const NavBar = () => {
+  return (
+    <>
+      <Nav>
+        <NavBarContainer>
+          <NavLogo>Medianet</NavLogo>
+        </NavBarContainer>
+      </Nav>
+    </>
+  );
+};
+export default NavBar;
