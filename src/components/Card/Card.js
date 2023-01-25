76d6ce2f1263f861ac0eff43403d49de25a4ec18
DiameCjs <diame.cjs@gmail.com>
2023-01-25 17:38:21 -0500

    cards responsive

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -11,146 +11,145 @@ const Card = () => {
           height={300}
         />
       </div>
-      <div id="card">
-        <h1 id="name">25 Megas Internet</h1>
-        <div id="conjuntoLi">
-          <h5>
-            <li id="liCard">- Misma velocidad carga y desarga</li>
-            <li id="liCard">- Instalación bonificada</li>
-          </h5>
-          <h5>
-            <li id="liCard">-Televisión premium x $3100</li>
-            <li id="liCard">- Incluidos packs premium</li>{" "}
-          </h5>
+      <div id="column">
+        <div id="card">
+          <h1 id="name">25 Megas Internet</h1>
+          <div id="conjuntoLi">
+            <h5>
+              <li id="liCard">- Misma velocidad carga y desarga</li>
+              <li id="liCard">- Instalación bonificada</li>
+            </h5>
+            <h5>
+              <li id="liCard">-Televisión premium x $3100</li>
+              <li id="liCard">- Incluidos packs premium</li>{" "}
+            </h5>
+          </div>
+          <div id="imagenes">
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/5556/5556367.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/8313/8313119.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/1321/1321688.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/159/159599.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+          </div>
         </div>
-        <div id="imagenes">
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/5556/5556367.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/8313/8313119.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/1321/1321688.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/159/159599.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-        </div>
-      </div>
 
-      <div id="card">
-        <h1 id="name">50 Megas Internet</h1>
-        <h1 id="precio">$1200</h1>
-        <div id="conjuntoLi">
-          <h5>
-            <li id="liCard">- Misma velocidad carga y descarga</li>
-            <li id="liCard">- Instalación bonificada</li>
-          </h5>
-          <h5>
-            <li id="liCard">-Televisión premium x $3100</li>
-            <li id="liCard">- Incluidos packs premium</li>{" "}
-          </h5>
+        <div id="card">
+          <h1 id="name">50 Megas Internet</h1>
+          <h1 id="precio">$1200</h1>
+          <div id="conjuntoLi">
+            <h5>
+              <li id="liCard">- Misma velocidad carga y descarga</li>
+              <li id="liCard">- Instalación bonificada</li>
+            </h5>
+            <h5>
+              <li id="liCard">-Televisión premium x $3100</li>
+              <li id="liCard">- Incluidos packs premium</li>{" "}
+            </h5>
+          </div>
+          <div id="imagenes">
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/5556/5556367.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/8313/8313119.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/1321/1321688.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/159/159599.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+          </div>
         </div>
-        <div id="imagenes">
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/5556/5556367.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/8313/8313119.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/1321/1321688.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/159/159599.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-        </div>
-      </div>
 
-      <div id="card">
-        <h1 id="name">100 Megas Internet</h1>
-        <h1 id="precio">$1700</h1>
-        <div id="conjuntoLi">
-          <h5>
-            <li id="liCard">- Misma velocidad carga y descarga</li>
-            <li id="liCard">- Instalación bonificada</li>
-          </h5>
-          <h5>
-            <li id="liCard">-Televisión premium x $3100</li>
-            <li id="liCard">- Incluidos packs premium</li>{" "}
-          </h5>
+        <div id="card">
+          <h1 id="name">100 Megas Internet</h1>
+          <h1 id="precio">$1700</h1>
+          <div id="conjuntoLi">
+            <h5>
+              <li id="liCard">- Misma velocidad carga y descarga</li>
+              <li id="liCard">- Instalación bonificada</li>
+            </h5>
+            <h5>
+              <li id="liCard">-Televisión premium x $3100</li>
+              <li id="liCard">- Incluidos packs premium</li>{" "}
+            </h5>
+          </div>
+          <div id="imagenes">
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/5556/5556367.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/8313/8313119.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/1321/1321688.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+            <img
+              id="iconoCard"
+              src="https://cdn-icons-png.flaticon.com/512/159/159599.png"
+              alt="fibra optica"
+              width={40}
+              height={40}
+            />
+          </div>
         </div>
-        <div id="imagenes">
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/5556/5556367.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/8313/8313119.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/1321/1321688.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-          <img
-            id="iconoCard"
-            src="https://cdn-icons-png.flaticon.com/512/159/159599.png"
-            alt="fibra optica"
-            width={40}
-            height={40}
-          />
-        </div>
+        
       </div>
-      <div>
-      <a href="https://wa.me/573148880850" target="_blank">
-      <button id="contactanos">
-        Contactanos!
-      </button>
-      </a>
-      </div>
+        <a href="https://wa.me/573148880850" target="_blank">
+          <button id="contactanos">Contactanos!</button>
+        </a>
     </div>
   );
 };
